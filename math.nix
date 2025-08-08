773ccdb3e51e2ef3d8c3b5c7e569c2b04f8817ac
randomdude16671 <randomdude16671@proton.me>
2025-08-08 20:30:20 +0530

    Init

---
+++ math.nix
@@ -0,0 +1,29 @@
+let
+  inherit (builtins)
+    div
+    mod
+    foldl'
+    abs
+    length
+    ;
+in
+rec {
+  add = (a: (b: a + b));
+  sub = (a: (b: a - b));
+  mul = (a: (b: a * b));
+
+  div_ = (a: (b: if b == 0 then throw "zero div error" else div a b));
+  mod_ = (a: (b: mod a b));
+
+  pow = base: exponent: if exponent == 0 then 1 else base * (pow base (exponent - 1));
+  factorial = n: if n <= 1 then 1 else n * factorial (n - 1);
+  # HCF == highest common factor (greatest common divisor)
+  hcf = (a: (b: if b == 0 then a else hcf b (mod a b)));
+  lcm = a: b: (abs (a * b)) / (hcf a b);
+
+  listLcm = ns: foldl' lcm 1 ns;
+
+  sum = ns: foldl' (add) 0 ns;
+  product = ns: foldl' (mul) 0 ns;
+  mean_avg = ns: if length ns == 0 then 0 else div_ (sum ns) (length ns); # mean avg returned as integer
+}
